3147d1f8272f88e8f3231fdc138498598ee4432e
swoldev <swoldev@proton.me>
2025-05-25 11:39:34 +0100

    connected db to integration tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-use zero2prod::run;
+use std::net::TcpListener;
+
+use zero2prod::{configuration::get_configuration, run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
